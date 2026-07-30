df4c699b0d4d0222f40630e4866ab55562be7b77
harristotle1987 <63924354+harristotle1987@users.noreply.github.com>
2026-07-30 15:10:10 +0100

    refactor: add error handling for Firebase credentials
    Safely parse the `FIREBASE_SERVICE_ACCOUNT` environment variable to prevent application crashes due to malformed JSON. Fallback to default initialization if parsing fails.

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -3,10 +3,21 @@ import { getFirestore } from 'firebase-admin/firestore';
 
 if (!getApps().length) {
     if (process.env.FIREBASE_SERVICE_ACCOUNT) {
-        const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-        initializeApp({
-            credential: cert(serviceAccount),
-        });
+        
+        let serviceAccount;
+        try {
+            serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+        } catch (err) {
+            console.warn("FIREBASE_SERVICE_ACCOUNT is not valid JSON. Ignoring.");
+        }
+        if (serviceAccount) {
+            initializeApp({
+                credential: cert(serviceAccount),
+            });
+        } else {
+            initializeApp();
+        }
+
     } else {
         initializeApp({
             /* Application default credentials */
